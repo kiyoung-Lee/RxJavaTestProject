df779cf89079e3fa67c43c1eb6341ded7dcb0363
KiyoungLee <eevee300@gmail.com>
2019-02-11 20:52:28 +0900

    add Sample2

--- app/src/test/java/com/example/luke/rxjavastudy/RxJavaSample2.java
+++ app/src/test/java/com/example/luke/rxjavastudy/RxJavaSample2.java
@@ -1,6 +1,8 @@
 package com.example.luke.rxjavastudy;
 
 import org.junit.Test;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 
 
 import io.reactivex.Observable;
@@ -54,5 +56,74 @@ public class RxJavaSample2 {
                 (Throwable t) -> t.printStackTrace(),
                 () -> System.out.println("OnComplete!!")
         );
+
+        stream.subscribe(
+                System.out::println,
+                Throwable::printStackTrace
+        );
     }
+
+    @Test
+    public void SubscriptionTest() {
+        Observable<String> sampleStream = Observable.create(s -> {
+            s.onNext("test");
+            s.onComplete();
+        });
+
+        Subscription subscription = (Subscription) sampleStream.subscribe();
+        subscription.cancel();
+
+        Disposable disposable = sampleStream.subscribe();
+        disposable.dispose();
+    }
+
+    @Test
+    public void SubscriberTest() {
+        Subscriber<String> subscriber = new Subscriber<String>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                System.out.println("Print!!" + s);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+
+
+        Observable<String> subscriberStream = Observable.create(s -> {
+            s.onNext("Test");
+            s.onComplete();
+        });
+
+        // RxJava2 에서는 안됨..
+//        subscriberStream.subscribe(subscriber);
+    }
+
+    @Test
+    public void cacheTest() {
+        Observable<String> cacheStream = Observable.<String>create(s ->{
+            System.out.println("cacheStart");
+            s.onNext("test1");
+            s.onNext("test2");
+            s.onComplete();
+        }).cache();
+
+        System.out.println("Start");
+        cacheStream.subscribe(s -> System.out.println(s));
+        cacheStream.subscribe(s -> System.out.println(s));
+        System.out.println("End");
+    }
+
 }
