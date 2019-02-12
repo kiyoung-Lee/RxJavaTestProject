32d4a8895af0b534f194df7c0f1c3d3f812a76df
KiyoungLee <eevee300@gmail.com>
2019-02-12 21:09:54 +0900

    add subject sample

--- app/src/test/java/com/example/luke/rxjavastudy/RxJavaSample2.java
+++ app/src/test/java/com/example/luke/rxjavastudy/RxJavaSample2.java
@@ -8,6 +8,10 @@ import org.reactivestreams.Subscription;
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.subjects.AsyncSubject;
+import io.reactivex.subjects.BehaviorSubject;
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.ReplaySubject;
 
 public class RxJavaSample2 {
 
@@ -126,4 +130,61 @@ public class RxJavaSample2 {
         System.out.println("End");
     }
 
+
+    @Test
+    public void subjectTest() {
+        PublishSubject<String> subject = PublishSubject.create();
+
+        subject.onNext("test1");
+
+        Observable subjectObservable = subject;
+
+        subjectObservable.subscribe(s -> System.out.println(s));
+
+        subject.onNext("test2");
+        subject.onNext("test3");
+    }
+
+    @Test
+    public void asyncSubjectTest() {
+        AsyncSubject<String> asyncSubject = AsyncSubject.create();
+
+        asyncSubject.onNext("test1");
+        asyncSubject.onNext("test2");
+        asyncSubject.onNext("test3");
+
+        Observable<String> asyncSubjectObservable = asyncSubject;
+
+        asyncSubjectObservable.subscribe(s -> System.out.println(s));
+
+        asyncSubject.onComplete();
+        asyncSubject.onNext("test4");
+    }
+
+    @Test
+    public void behaviorSubjectTest() {
+        BehaviorSubject<String> behaviorSubject = BehaviorSubject.create();
+
+        behaviorSubject.onNext("test1");
+        behaviorSubject.onNext("test2");
+        behaviorSubject.onNext("test3");
+
+        Observable<String> behaviorSubjectObservalbe = behaviorSubject;
+
+        behaviorSubjectObservalbe.subscribe(s -> System.out.println(s));
+
+        behaviorSubject.onNext("test4");
+        behaviorSubject.onNext("test5");
+    }
+
+    @Test
+    public void replaySubjectTest() {
+        ReplaySubject<String> replaySubject = ReplaySubject.create();
+
+        replaySubject.onNext("test1");
+        replaySubject.onNext("test2");
+
+        Observable<String> replaySubjectObservable = replaySubject;
+
+    }
 }
